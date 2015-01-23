ca9243037b3ce7b55a7283a17ad10fbae8169e55
CNexus <me@cnexus.co>
2015-01-23 17:17:02 -0500

    Modify parameters to fit updated data types

--- main/src/me/carlosgonzales/scalereader/receiver/DataWriter.java
+++ main/src/me/carlosgonzales/scalereader/receiver/DataWriter.java
@@ -1,8 +1,8 @@
 package me.carlosgonzales.scalereader.receiver;
 
 import java.io.*;
-import java.util.Map;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * Created by Carlos on 1/23/2015.
@@ -13,22 +13,29 @@ public class DataWriter {
 		this.file = file;
 	}
 
-	public void writeData(Map<Double, String> values){
+	public void writeData(ArrayList<LinkedList<String>> values){
 		PrintWriter writer = null;
 
 		try {
 			writer = new PrintWriter(new BufferedWriter(new FileWriter(file, file.exists())));
 
-			Set<Double> keys = values.keySet();
+			LinkedList<String> headers = values.get(0);
 
-			for(Double d: keys){
-				String timestamp = values.get(d);
-				writer.println(d + "\t" + timestamp);
+			for(int i = 1; i < values.size(); i++){
+				writeLine(writer, values.get(i));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}finally{
 			writer.close();
 		}
+	}
+
+	private void writeLine(PrintWriter pw, LinkedList<String> entry){
+		StringBuffer line = new StringBuffer();
+		for(int j = 0; j < entry.size(); j++)
+			line.append(entry.get(j) + "\t");
+
+		pw.println(line.toString().trim());
 	}
 }
