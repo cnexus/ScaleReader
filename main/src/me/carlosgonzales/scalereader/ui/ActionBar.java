f2da5aca4bb4b2b607e6a51d13ae530dc4fdc660
CNexus <me@cnexus.co>
2015-01-23 10:36:58 -0500

    Add ui files

--- main/src/me/carlosgonzales/scalereader/ui/ActionBar.java
+++ main/src/me/carlosgonzales/scalereader/ui/ActionBar.java
@@ -6,15 +6,23 @@ import javax.swing.*;
  * Created by Carlos on 1/23/2015.
  */
 public class ActionBar extends JMenuBar{
-	private static final String[] titleBars = {" File ", " Save "};
+	private static final String[] TITLES = {" File ", " Save "};
 
 	private ScaleReaderGUI parent;
-	private JMenuItem[] items;
+	private JMenu[] menus;
 
 	public ActionBar(ScaleReaderGUI parent){
 		this.parent = parent;
-		this.setBorderPainted(true);
+		initMenu();
+	}
 
+	private void initMenu(){
+		setBorderPainted(true);
+		menus = new JMenu[TITLES.length];
 
+		for(int i = 0; i < TITLES.length; i++){
+			menus[i] = new JMenu(TITLES[i]);
+			this.add(menus[i]);
+		}
 	}
 }
--- main/src/me/carlosgonzales/scalereader/ui/ScaleReaderGUI.java
+++ main/src/me/carlosgonzales/scalereader/ui/ScaleReaderGUI.java
@@ -1,13 +1,23 @@
 package me.carlosgonzales.scalereader.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Carlos on 1/23/2015.
  */
 public class ScaleReaderGUI extends JFrame {
-	public ScaleReaderGUI(){
+	private static final String NAME = "ScaleReader";
 
+	private ScaleReaderGUI(String name){
+		super(name);
+		this.setJMenuBar(new ActionBar(this));
+		this.setSize(500, 400);
+		this.setResizable(false);
+	}
+
+	public ScaleReaderGUI(){
+		this(NAME);
 	}
 
 	public static void main(String[] args){
@@ -19,6 +29,22 @@ public class ScaleReaderGUI extends JFrame {
 	}
 
 	private static void createAndShowGUI(){
+		try {
+			Font f = new Font("Tahoma", Font.PLAIN, 18);
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+			UIManager.getLookAndFeelDefaults()
+					.put("defaultFont", f);
 
+			ScaleReaderGUI main = new ScaleReaderGUI();
+			main.setVisible(true);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
 	}
 }
